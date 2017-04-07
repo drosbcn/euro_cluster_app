e16a448c4880a08a95bb217eb21d088f39be4829
drosbcn <david.rosenfeld@barcelonagse.eu>
2017-04-08 01:22:44 +0200

    upload euro_cluster_app

---
+++ macro_convergence.R
@@ -0,0 +1,62 @@
+
+
+macro_pca <- function(year_select, k, variables){
+  
+  setwd(getwd())
+  
+  library(plyr)
+  library(dplyr)
+  library(ggplot2)
+  
+  macro_dataset <- read.csv("macro_dataset.csv")[,-1]
+  
+  dataset <- macro_dataset %>%
+    filter(Year == year_select) %>%
+    select_(.dots = c("LOCATION", variables))
+  
+  dataset[,2:ncol(dataset)] <- scale(dataset[,2:ncol(dataset)])
+  
+  for(i in 2:ncol(dataset)){
+    dataset[,i][is.na(dataset[,i])] = mean(dataset[,i], na.rm = TRUE)
+  }
+  
+  rownames(dataset) <- dataset[,1]
+  
+  dataset_dist <- dist(dataset[,2:ncol(dataset)])
+  dataset_clust <- hclust(dataset_dist, method = "average")
+  dendrogram <- plot(dataset_clust)
+  cut_tree <- as.matrix(cutree(dataset_clust, k = k), ncol = 1)
+  
+  dataset <- cbind(dataset, cut_tree)
+  
+  svd_data <- svd(dataset[, 2:(ncol(dataset) - 1)])
+  rpc <- data.frame(svd_data$u %*% diag(svd_data$d))
+  csc <- data.frame(svd_data$v)
+  
+  pve <- 100*svd_data$d^2/sum(svd_data$d^2)
+  
+  pca_plot <- ggplot(NULL, aes(x = X1, y = X2)) +
+    geom_point(data = rpc, size = 8, alpha = 0.7, aes(col = as.factor(dataset$cut_tree),
+                                                      stroke = 1)) +
+    geom_text(data = rpc, aes(label = dataset[,1])) +
+    geom_segment(data = csc, aes(x = 0, xend = 5* csc[,1], y = 0, yend = 5*csc[,2]),
+                 arrow = arrow(length = unit(0.2,"cm"))) +
+    geom_text(data = csc, aes(x = 5.5* csc[,1], y = 5.5*csc[,2], 
+                              label = colnames(dataset[,2:(ncol(dataset) - 1)]))) +
+    # Make the background blank, get rid of grid lines, but keep axis lines and make the axis text bold
+    theme(panel.background = element_blank(), panel.grid = element_blank(), axis.line = element_line(color = "black"), 
+          axis.ticks = element_line(color = "black"), axis.title = element_text(face = "bold"), 
+          axis.text = element_text(face = "bold"), legend.position = "none") +
+    # Add horizontal and vertical dotted lines at 0
+    geom_hline(yintercept = 0, lty = 2, alpha = 0.5) +
+    geom_vline(xintercept = 0, lty = 2, alpha = 0.5) +
+    ggtitle(paste0("Year: ", year_select, 
+                   " (total variance explained: ", round(pve[1] + pve[2]), "%)")) + 
+    theme(plot.title = element_text(hjust = 0.5, face = "bold")) +
+    scale_colour_brewer(type = "qual") +
+    xlab(paste0("First Principal Component (variance explained: ", round(pve[1]), "%)")) +
+    ylab(paste0("Second Principal Component (variance explained: ", round(pve[2]), "%)"))
+  
+  return(list(dendrogram = dendrogram, pca_plot = pca_plot))
+}
+
---
+++ ui.R
@@ -0,0 +1,64 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(markdown)
+library(DT)
+
+navbarPage("European economic integration: PCA and clustering",
+           tabPanel("Modeling",
+                    sidebarLayout(
+                      sidebarPanel(
+                        sliderInput("year_select",
+                                    "Year:",
+                                    min = 1995,
+                                    max = 2014,
+                                    value = 2014),
+                        sliderInput("k",
+                                    "Number of clusters:",
+                                    min = 1,
+                                    max = 8,
+                                    value = 5),
+                        checkboxGroupInput("variables", "Variables:", c("GDP" = "GDP", "GDP growth" = "gdp_growth", 
+                                                                        "Average annual wage" = "average_annual_wage", "CPI inflation" = "cpi",
+                                                                        "Current Account Balance" = "current_account_balance",
+                                                                        "Productivity" = "productivity", "Unemployment rate" = "unemployment_rate",
+                                                                        "Gross Expenditure on R&D (GERD)" = "GERD", 
+                                                                        "Government fiscal balance" = "govt_fiscal_balance", 
+                                                                        "Government debt" = "govt_debt", 
+                                                                        "Current account flows" = "current_account_flows", 
+                                                                        "Investment flows" = "investment_flows"),
+                                           selected = c("GDP", "gdp_growth", "average_annual_wage", "cpi",
+                                                        "current_account_balance", "unemployment_rate",
+                                                        "govt_fiscal_balance", "govt_debt", "current_account_flows"))
+                      ),
+                      mainPanel(
+                        plotOutput("pca_plot"),
+                        plotOutput("dendrogram")
+                      ))
+           ),
+           tags$head(tags$style(HTML('
+                                     .irs-bar {
+                                     background: #fb370b;
+                                     border-top:#fb370b;
+                                     border-bottom:#fb370b;
+                                     }
+                                     .irs-bar-edge {
+                                     border: #fb370b;
+                                     background: #fb370b;
+                                     
+                                     }
+                                     .irs-single {
+                                     background: #fb370b;
+                                     }
+                                     
+                                     '))),
+           tabPanel("Report",
+                    includeHTML("Report.html")
+           ))
